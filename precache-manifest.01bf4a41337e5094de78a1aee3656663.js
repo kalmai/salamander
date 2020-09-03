ea8d9729bd7eafb4f32bd039dfa020a964e7e334
kalmai <leozverres@gmail.com>
2020-09-02 22:07:38 -0400

    Updates

---
+++ precache-manifest.01bf4a41337e5094de78a1aee3656663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17e5f6affd6a9104be936daf565231d",
+    "url": "/kalmai.github.io/my-app/index.html"
+  },
+  {
+    "revision": "04d9ff2b200f7406521f",
+    "url": "/kalmai.github.io/my-app/static/css/main.32feaaac.chunk.css"
+  },
+  {
+    "revision": "36b0eac9b16e55a4c3b6",
+    "url": "/kalmai.github.io/my-app/static/js/2.77ac4fb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kalmai.github.io/my-app/static/js/2.77ac4fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d9ff2b200f7406521f",
+    "url": "/kalmai.github.io/my-app/static/js/main.9f8f42b8.chunk.js"
+  },
+  {
+    "revision": "5625115209dde1bbea9c",
+    "url": "/kalmai.github.io/my-app/static/js/runtime-main.3c025696.js"
+  }
+]);
